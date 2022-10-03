ce8a1b093258a36a87bc810d2bceaca009a14bab
ahmed22553 <105362373+ahmed22553@users.noreply.github.com>
2022-10-03 11:27:08 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let imo = document.querySelector(".imo")
+let container = document.querySelector(".container")
+// console.log(imo)
+function phone(xx){
+    imo.src=xx
+}
+function color(ff){
+    container.style.background = ff
+
+}
